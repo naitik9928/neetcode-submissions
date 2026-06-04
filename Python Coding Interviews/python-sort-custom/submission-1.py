08433744043475845c35a154b6dd750565fb222c
Naitik <naitikkumawat5100@gmail.com>
2026-06-04 15:29:30 +0530

    Add: python-sort-custom - submission-1

---
+++ Python Coding Interviews/python-sort-custom/submission-1.py
@@ -0,0 +1,18 @@
+from typing import List
+def len_word(word):
+    return len(word)
+
+def sort_words(words: List[str]) -> List[str]:
+    words.sort(reverse=True,key=len_word)
+    return words
+def absolute_value(value):
+    return abs(value)
+def sort_numbers(numbers: List[int]) -> List[int]:
+    numbers.sort(key=absolute_value)
+    return numbers
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
